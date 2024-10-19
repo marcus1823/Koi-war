643d04aea67fe851d6ccf275a9bef8f020432926
Truong Minh Tien <145823751+marcus1823@users.noreply.github.com>
2024-10-19 14:54:51 +0700

    update allow any origin

--- koi-war-backend/src/config/cors.config.ts
+++ koi-war-backend/src/config/cors.config.ts
@@ -1,10 +1,13 @@
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:8081',  'https://koiwar.site', ];
+// const allowedOrigins = ['http://localhost:3000', 'http://localhost:8081',  'https://koiwar.site', ];
 export const corsOptions = {
-    origin: function (origin : any, callback: any) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
+
+    origin: '*',  // Cho phép tất cả các origins
+
+    // origin: function (origin : any, callback: any) {
+    //     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error('Not allowed by CORS'));
+    //     }
+    // }
 };
